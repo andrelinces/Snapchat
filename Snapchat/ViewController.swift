f03c1c8b838ee208aeab2bfd060678df819c55fa
Andre Linces <andrelinces@yahoo.com.br>
2021-09-21 10:45:33 -0300

    Criando interface snapchat clone.

--- Snapchat/ViewController.swift
+++ Snapchat/ViewController.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
-
+import Firebase
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+    
+        
         // Do any additional setup after loading the view.
     }
 
