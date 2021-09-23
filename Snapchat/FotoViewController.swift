b1f9481ce8c6546f19c3195684ea9a6c73c356dc
Andre Linces <andrelinces@yahoo.com.br>
2021-09-23 09:46:57 -0300

    Criando classes para os layouts snaps e foto.

---
+++ Snapchat/FotoViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FotoViewController.swift
+//  Snapchat
+//
+//  Created by Andre Linces on 23/09/21.
+//
+
+import UIKit
+
+class FotoViewController {
+    
+    
+}
---
+++ Snapchat/SnapsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SnapsViewController.swift
+//  Snapchat
+//
+//  Created by Andre Linces on 23/09/21.
+//
+
+import UIKit
+
+class SnapsViewController {
+    
+    
+    
+}
