c90655a4f032ee49cf784dee02d66bf409cebf4e
Andre Linces <andrelinces@yahoo.com.br>
2021-09-24 19:16:53 -0300

    Corrigido erro para acessar layout de adicionar snap.

--- Snapchat/FotoViewController.swift
+++ Snapchat/FotoViewController.swift
@@ -9,6 +9,22 @@ import UIKit
 
 class FotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    //@IBOutlet weak var imagem: UIImageView!
+    //@IBOutlet weak var descricao: UITextField!
+    /*
     var imagePicker = UIImagePickerController()
+    
+    @IBAction func selecionaFoto(_ sender: Any) {
+        
+        imagePicker.sourceType = .savedPhotosAlbum
+        present(imagePicker, animated: true, completion: nil )
+    }
+    */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       // imagePicker.delegate = self
+        
+    }
     
 }
