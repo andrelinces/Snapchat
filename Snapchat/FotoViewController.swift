859e9563c14e84ac32a1be980ea5f6ebfee1b59e
Andre Linces <andrelinces@yahoo.com.br>
2021-09-29 08:48:42 -0300

    imagem selecionada salva no firebase storage. Método para customizar e desabilitar e habilitar o botão.

--- Snapchat/FotoViewController.swift
+++ Snapchat/FotoViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseStorage
 
 class FotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -13,9 +14,49 @@ class FotoViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     
     @IBOutlet weak var descricao: UITextField!
     
-    
+    @IBOutlet weak var botaoProximo: UIButton!
     
     var imagePicker = UIImagePickerController()
+    
+    
+    @IBAction func proximoPasso(_ sender: Any) {
+        //Desabilitando o botão quando pressionado exibindo mensagem enquanto carrega a foto selecionada.
+        self.botaoProximo.isEnabled = false
+        self.botaoProximo.setTitle("Carregando...", for: .normal)
+        
+        //Salvando os dados no firebase, instanciando objeto
+        let armazenamento = Storage.storage().reference()
+        //Criando pasta para organizar as imagens
+        let imagens = armazenamento.child("imagens")
+        
+        //recuperar a imagem
+        if let imagemSelecionada = image.image {
+            
+            if let imagemDados = imagemSelecionada.jpegData(compressionQuality: 0.5) {
+            imagens.child("imagem.jpg").putData(imagemDados, metadata: nil) { metaDados, erro in
+               
+                //Caso não ocorra nenhum erro, vai exibir sucesso do print
+                if erro == nil {
+                    
+                    print("Sucesso ao fazer upload do arquivo!")
+                    //Habilitando o botão após salvar a imagem e alterando o texto.
+                    self.botaoProximo.isEnabled = true
+                    self.botaoProximo.setTitle("Próximo", for: .normal)
+                    
+                }else{
+                    
+                    print("Erro ao fazer upload do Arquivo")
+                    
+                }
+                
+            }
+          }
+            
+        }
+        
+        
+        
+    }
     
     @IBAction func selecionarFoto(_ sender: Any) {
         
@@ -24,11 +65,28 @@ class FotoViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         
     }
    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        //print para testar se esta selecionando a foto corretamente.
+        //print(info)
+        
+        //Utilizando o dicionário (info) para recuperar a foto selecionada pelo usuário.
+        let imagemRecuperada = info [ UIImagePickerController.InfoKey.originalImage ] as! UIImage
+        //recuperando imagem e exibindo na UIImageView.
+        image.image = imagemRecuperada
+        //objeto utilizado para fechar a biblioteca quando o usuário selecionar a foto.
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         imagePicker.delegate = self
+        
+        //Desabilita o botão ao carregar a tela, até que o usuário selecione uma foto.
+        botaoProximo.isEnabled = false
+        botaoProximo.backgroundColor = UIColor.gray
         
     }
     
