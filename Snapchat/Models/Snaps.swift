1bb959a94b33f367417dbcc176ebd263d30596f1
Andre Linces <andrelinces@yahoo.com.br>
2021-10-02 19:35:43 -0300

    Criando layout para receber os snaps.

---
+++ Snapchat/Models/Snaps.swift
@@ -0,0 +1,19 @@
+//
+//  Snaps.swift
+//  Snapchat
+//
+//  Created by Andre Linces on 02/10/21.
+//
+
+import UIKit
+
+class Snaps {
+    
+    var identificador = ""
+    var nome = ""
+    var de = ""
+    var descricao = ""
+    var urlImagem = ""
+    var idImagem = ""
+    
+}
--- Snapchat/ViewsControllers/SnapsViewController.swift
+++ Snapchat/ViewsControllers/SnapsViewController.swift
@@ -9,7 +9,14 @@ import UIKit
 import FirebaseDatabase
 import FirebaseAuth
 
-class SnapsViewController: UIViewController {
+class SnapsViewController:
+    UIViewController,
+    UITableViewDelegate,
+    UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var snaps: [Snaps] = []
     
     @IBAction func sair(_ sender: Any) {
         
@@ -41,16 +48,74 @@ class SnapsViewController: UIViewController {
             let database = Database.database().reference()
             let usuarios = database.child("usuarios")
             
-            let snaps = usuarios.child( idUsuarioLogado ).child("snaps") 
+            let snaps = usuarios.child( idUsuarioLogado ).child("snaps")
             
             //cria ouvinte para o Snaps
+            snaps.observe( DataEventType.childAdded) { snapshot in
+                //print para testar a exibição no log
+                //print(snapshot)
+                
+                let dados = snapshot.value as? NSDictionary
+                
+                //criando referencia do objeto com a classe Snaps
+                let snap = Snaps()
+                
+                snap.identificador = snapshot.key
+                snap.nome = dados?["nome"] as! String
+                snap.descricao = dados?["descricao"] as! String
+                snap.urlImagem = dados?["urlImagem"] as! String
+                snap.idImagem = dados?["idImagem"] as! String
+                
+                //Montando a quantidade de snaps recebidos
+                self.snaps.append( snap )
+                //recarregando a tabela
+                self.tableView.reloadData()
+                //print para testar se o objeto snap está recebendo todos os dados recuperados do snap.
+                print(self.snaps)
+                
+            }
             
             
-        }
+        }//fim do if idUsuarioLogado
         
         
     }
-    
-      
+    /*
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    */
+    //Quantidade de snaps por linha
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        let totalSnaps = self.snaps.count
+        if totalSnaps == 0 {
+            return 1
+        }
+        
+        //Retornar a quantidade de linhas necessárias de acordo com a quantidade de snaps
+        return totalSnaps
+    }
+    //Montando a celula
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let celula = tableView.dequeueReusableCell(withIdentifier: "celulaReuso", for: indexPath)
+        
+        let totalSnaps = snaps.count
+        
+        if totalSnaps == 0 {
+            
+            //Caso não tenha nenhum snap, será exibida mensagem na célula.
+            celula.textLabel?.text = "Nenhum snap para voce :-)"
+        }else{
+            //Caso tenha o snap será montado os snaps na tela.
+            let snap = self.snaps[ indexPath.row ]
+            celula.textLabel?.text = snap.nome
+            
+        }
+        
+        return celula
+        
+    }
     
 }
